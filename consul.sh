206259ac4e2623f089fbb037b5dc870a8abe7b5c
Nasir Nooruddin <nasir@nasir.id>
2023-12-17 21:48:01 +0700

    Update consul.sh

--- consul.sh
+++ consul.sh
@@ -1,53 +1,49 @@
-sudo yum update -y
-sudo adduser consul --shell=/bin/false-no-create-home
-sudo mkdir -p /data/consul /etc/consul/consul.d
-sudo chown -R consul:consul /data/consul /etc/consul
-sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
-sudo yum -y install consul
-sudo cat <<EOF > /etc/systemd/system/consul.service
-[Unit]
-Description="HashiCorp Consul - A service mesh solution"
-Documentation=https://www.consul.io/
-Requires=network-online.target
-After=network-online.target
-ConditionFileNotEmpty=/etc/consul.d/consul.json
-
-[Service]
-User=consul
-Group=consul
-ExecStart=/usr/bin/consul agent -config-dir=/etc/consul.d/
-ExecReload=/usr/bin/consul reload
-ExecStop=/usr/bin/consul leave
-KillMode=process
-Restart=on-failure
-LimitNOFILE=65536
-
-[Install]
-WantedBy=multi-user.target
-EOF
-mv /etc/consul.d/consul.hcl /etc/consul.d/consul.hcl_bak
-sudo cat <<EOF > /etc/consul.d/consul.json
+apt update -y
+apt install unzip -y
+cd /usr/local/bin
+sudo curl -o consul.zip https://releases.hashicorp.com/consul/1.6.0/consul_1.6.0_linux_amd64.zip
+unzip consul.zip
+sudo mkdir -p /etc/consul.d/scripts
+sudo mkdir /var/consul
+sudo cat <<EOF > /etc/consul.d/config.json
 {
-    "datacenter": "nasir-dc",
-    "bootstrap_expect": 1,
-    "data_dir": "/data/consul",
-    "log_level": "INFO",
-    "node_name": "consul-2",
-    "ui_config": {
-                "enabled": true,
-                "content_path":"/ui/"
-        },
-    "server": true,
-    "leave_on_terminate": true,
-    "encrypt": "V++fphCcR8ppDG1Y0S2wGyDVWipJcZjJqKKNSJkEaUg=",
-    "rejoin_after_leave": true,
-    "retry_join": ["consul-1.local:8301", "consul-2.local:8301", "consul-3.local:8301"],
-    "enable_script_checks": true,
-    "advertise_addr": "34.87.58.210",
-    "bind_addr": "0.0.0.0",
+    "bootstrap_expect": 3,
     "client_addr": "0.0.0.0",
-    "log_file": "/var/log/consul/consul.log"
+    "datacenter": "nasir-dc",
+    "data_dir": "/var/consul",
+    "domain": "consul",
+    "enable_script_checks": true,
+    "dns_config": {
+        "enable_truncate": true,
+        "only_passing": true
+    },
+    "enable_syslog": true,
+    "encrypt": "QrFGmdDhqOX4WkhSUbQpx16f15NgSMfBta++jsH/qTE=",
+    "leave_on_terminate": true,
+    "log_level": "INFO",
+    "rejoin_after_leave": true,
+    "server": true,
+    "start_join": [
+        "172.16.0.17",
+        "172.16.0.18",
+        "172.16.0.19"
+    ],
+    "ui": true
 }
 EOF
-sudo service consul start
+sudo cat <<EOF > /etc/systemd/system/consul.service
+[Unit]
+Description=Consul Startup process
+After=network.target
+
+[Service]
+Type=simple
+ExecStart=/bin/bash -c '/usr/local/bin/consul agent -config-dir /etc/consul.d/'
+TimeoutStartSec=0
+
+[Install]
+WantedBy=default.target
+EOF
+sudo systemctl daemon-reload
+sudo systemctl enable --now consul
+sudo systemctl restart consul
