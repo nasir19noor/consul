ed9b007cb26bdb6f580287e67e45ec300c5508f2
Nasir Nooruddin <nasir@nasir.id>
2023-12-18 20:51:23 +0700

    Update consul_server.sh

--- docker/server/consul_server.sh
+++ docker/server/consul_server.sh
@@ -1,11 +1,4 @@
-#!/bin/bash
-
-# Start Consul Agent in Server mode
-consul agent -server \
-    -bind $PRIVATE_IP_ADDRESS \
-    -advertise $PRIVATE_IP_ADDRESS \
-    -node $NODE \
-    -client 0.0.0.0 \
-    -dns-port 53 \
-    -data-dir /data \
-    -ui -bootstrap
+docker compose down
+docker system prune -a
+docker compose build
+docker compose up -d
