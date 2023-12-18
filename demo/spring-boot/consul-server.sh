3ba6332d66d61aa44b6c35b423f107952c1dac21
Nasir Nooruddin <nasir@nasir.id>
2023-12-18 11:41:49 +0700

    modify repo

--- demo/spring-boot/consul-server.sh
+++ demo/spring-boot/consul-server.sh
@@ -1,2 +1,2 @@
-docker pull consul
+docker pull arm64v8/consul:latest
 docker run -d -p 8500:8500 -p 8600:8600/udp --name=consul-container consul agent -server -ui -node=server-1 -bootstrap-expect=1 -client=0.0.0.0
